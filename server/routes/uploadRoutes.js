91faa6af3a6ccd00c9216fb720651ae64d5c76d6
khallitos <carlphil9924@gmail.com>
2023-05-03 16:48:38 +0000

    er

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -1,13 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import {singleCargoDetails, uploadmusic ,songInfo,SingleSongInfo,getAllSongs,searchSong,deleteSong, editSong,userInfo,approveSong,getAllUnverifiedSongs,getSongsForAdmin,searchSongForAdmin,downloadCounter,getAdminDashboard,getTrendingSongs,getRandomSongs,getSignUrl} from "../controllers/uploadController.js";
+import {
+  singleCargoDetails,
+
+} from "../controllers/uploadController.js";
 
 router.route("/singleCargoDetails").post(singleCargoDetails);
-
-
-
-
-
 
 export default router;
